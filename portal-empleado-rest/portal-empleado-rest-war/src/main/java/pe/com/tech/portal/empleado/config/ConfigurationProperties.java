727a7dfbc505a44376ce2daf3e144de7b5579448
miguel.ochoa <miguel.ochoa.gallegos@gmail.com>
2018-09-21 15:49:43 -0500

    version profile dev

--- portal-empleado-rest/portal-empleado-rest-war/src/main/java/pe/com/tech/portal/empleado/config/ConfigurationProperties.java
+++ portal-empleado-rest/portal-empleado-rest-war/src/main/java/pe/com/tech/portal/empleado/config/ConfigurationProperties.java
@@ -1,11 +1,15 @@
 package pe.com.tech.portal.empleado.config;
 
+import static pe.com.tech.portal.empleado.constants.Perfiles.TEST;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
 @PropertySource(value = {"file:///tech/app/portal-empleado/rest/properties/config-${spring.profiles.active}.properties"})
+@Profile({TEST})
 public class ConfigurationProperties {
     
     @Value("${jdbc.jndi}")
